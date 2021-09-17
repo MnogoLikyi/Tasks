ae6ba2e1f28deece3124e8e02b6cd1aa5248fedf
Samvel <sammuki@Mail.ru>
2021-09-17 13:21:19 +0400

    done

---
+++ src/com/company/Calculators/BumerangCalculator.java
@@ -0,0 +1,33 @@
+package com.company.Calculators;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * bumerang calculator
+ * @author Samvel Ghazaryan
+ * */
+public class BumerangCalculator {
+
+
+    public List<int[]> getBumerangsFromArray(int[] arr) {
+        if (arr.length < 3 || arr == null) return null;
+
+        List<int[]> bumerangs = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex = 2;
+        int[] tempArr = new int[3];
+
+        while (endIndex != arr.length-1) // maybe length-1
+        {
+            int middlevalue = arr[startIndex +1];
+            if (arr[startIndex] == arr[endIndex] && arr[startIndex] != middlevalue && arr[endIndex]!= middlevalue) {
+                tempArr = Arrays.copyOfRange(arr, startIndex, endIndex+1);
+                bumerangs.add(tempArr);
+            }
+        startIndex++;
+        endIndex++;
+        }
+        return bumerangs;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import com.company.Calculators.BumerangCalculator;
+
+import javax.imageio.plugins.bmp.BMPImageWriteParam;
+import java.util.List;
+/**
+ * Main class
+ * @author Samvel Ghazaryan
+ * */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] arr = new int[]{1, 2, 1, 2, 5, 6, 5, 8, 6,8,7,7,7,5,7,9,8,2,3,2};
+        BumerangCalculator bmc = new BumerangCalculator();
+        List<int[]> bumerangs = bmc.getBumerangsFromArray(arr);
+
+        for (int[] items : bumerangs)
+        {
+            System.out.print("[");
+            for (int item : items)
+            {
+                System.out.print( item + ",");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+
+    }
+}
